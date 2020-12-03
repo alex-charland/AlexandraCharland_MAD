eb05caa2c9b0baf5a8b3a6e5da17c92b73318e51
alex-charland <66377521+alex-charland@users.noreply.github.com>
2020-12-02 22:32:47 -0700

    Project 2

--- MeanTime/app/src/main/java/com/example/meantime/MainActivity.java
+++ MeanTime/app/src/main/java/com/example/meantime/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.meantime;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -7,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
@@ -38,7 +41,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     boolean timerReady = false;
     boolean timerRunning = false;
     boolean timerPaused = false;
-    long pauseOffset;
+    long pauseOffset, time;
     int imageId = -1;
     String currentTimerVal;
     int currentTaskPosition;
@@ -54,33 +57,37 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         setContentView(R.layout.activity_main);
 
         timer = findViewById(R.id.timer);
-        timer.setText("00:00:00");
         chooseTask = findViewById(R.id.chooseTask);
         leftButton = findViewById(R.id.leftButton);
         rightButton = findViewById(R.id.rightButton);
         taskList = findViewById(R.id.taskList);
         taskdb = new DatabaseHelper(MainActivity.this);
         DisplayTasksInTable(taskdb);
+        timer.setText("00:00:00");
+
 
         //Fill the spinner
         fillTheSpinner();
 
         //ON CLICK LISTENERS
         chooseTask.setOnItemSelectedListener(this);
+        //DELETE TASK FROM TABLE
         taskList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
                 TaskInfo task = (TaskInfo) parent.getItemAtPosition(position);
                 boolean b = taskdb.deleteTask(task);
+                Toast.makeText(MainActivity.this,"Task Deleted",Toast.LENGTH_SHORT);
                 DisplayTasksInTable(taskdb);
                 fillTheSpinner();
                 return false;
             }
         });
+        //TIMER TICKING DISPLAY
         timer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
             @Override
             public void onChronometerTick(Chronometer chronometer) {
-                long time = SystemClock.elapsedRealtime() - chronometer.getBase();
+                time = SystemClock.elapsedRealtime() - chronometer.getBase();
                 int hours = (int)(time/3600000);
                 int minutes = (int)(time - hours*3600000)/60000;
                 int seconds= (int)(time - hours*3600000- minutes*60000)/1000 ;
@@ -95,31 +102,33 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             }
         });
     }
+
+    //CHECK IF TASK NAME ALREADY EXISTS
     private boolean checkName(String taskName){
         if(!stringTasks.contains(taskName)){
             return false;
         }
         return true;
     }
-
+    //FILL THE SPINNER WITH TASK NAMES FROM DATABASE
     private void fillTheSpinner() {
         populateSpinner();
         spinnerAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_item, stringTasks);
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
         chooseTask.setAdapter(spinnerAdapter);
     }
-
+    //RETRIEVE TASK NAMES FROM DATABASE
     private void populateSpinner(){
         stringTasks.clear();
         stringTasks.add("Select Task");
         stringTasks.addAll(taskdb.getTaskNames());
         stringTasks.add("Add Task");
     }
-
+    //SPINNER ITEM SELECTED HANDLER
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         selection = String.valueOf(parent.getItemAtPosition(position));
-        if (selection.equals("Add Task")){
+        if (selection.equals("Add Task")){//CREATE DIALOG FOR ADDING A TASK
             AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this, R.style.Theme_AppCompat_Dialog);
             View dialogLayout = getLayoutInflater().inflate(R.layout.select_task,null);
             builder.setView(dialogLayout);
@@ -129,10 +138,9 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 public void onClick(DialogInterface dialog, int which) {
                     TaskInfo task;
                     EditText newTask = dialogLayout.findViewById(R.id.newTask);
-                    if(!checkName(newTask.getText().toString())){
+                    if(!checkName(newTask.getText().toString()) && !newTask.getText().toString().equals("")){//ADD NEW TASK IF THE SAME TASK NAME DOES NOT EXIST IN THE DATABASE
                         task = new TaskInfo(-1,newTask.getText().toString(),0,0,0,0);
                         boolean success = taskdb.addTask(task);
-                        Toast.makeText(MainActivity.this, "Success = " + success, Toast.LENGTH_SHORT).show();
                         if (success) {
                             DisplayTasksInTable(taskdb);
                             fillTheSpinner();
@@ -142,7 +150,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                         }
                     }
                     else{
-                        Toast.makeText(MainActivity.this,"Task already exists",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this,"Task already exists or is empty",Toast.LENGTH_SHORT).show();
                     }
                 }
             });
@@ -162,39 +170,37 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         }
     }
-
+    //CHANGE THE STYLE OF THE BUTTONS FOR DIFFERENT TIMER STATES
     public void changeButtons(){
-        if(timerReady){
+        if(timerReady && !timerRunning && !timerPaused){ //Start
             leftButton.setImageResource(R.drawable.startgreen);
         }
-        else if(timerRunning){
+        else if(timerRunning && timerReady){ //Pause/End
             leftButton.setImageResource(R.drawable.pause);
             rightButton.setImageResource(R.drawable.endred);
         }
-        else if(timerPaused){
+        else if(timerPaused && !timerRunning){//Start/End
             leftButton.setImageResource(R.drawable.startgreen);
             rightButton.setImageResource(R.drawable.endred);
         }
-        else{
+        else if (!timerReady){//End
             leftButton.setImageResource(R.drawable.startgrey);
             rightButton.setImageResource(R.drawable.endgrey);
         }
     }
-
+    //ONCLICK BUTTON METHODS FOR STARTING/PAUSING THE TIMER
     public void startTimer(View view){
-        if(timerReady){
+        if(timerReady && !timerRunning){
             timer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
             timer.start();
             timerRunning = true;
-            timerReady = false;
             timerPaused = false;
             changeButtons();
         }
-        else if (timerRunning){
+        else if (timerRunning && timerReady){
             timer.stop();
             pauseOffset = SystemClock.elapsedRealtime() - timer.getBase();
             timerPaused = true;
-            timerReady = true;
             timerRunning = false;
             changeButtons();
         }
@@ -202,9 +208,9 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             Toast.makeText(MainActivity.this,"Please select an existing task or add a new task",Toast.LENGTH_SHORT).show();
         }
     }
-
+    //WHEN THE TIMER IS ENDED, CALCULATE THE AVERAGE TIME FOR THE TASK AND STORE IT IN THE DATABASE
     public void endTimer(View view){
-        if(timerRunning){
+        if(timerReady && (timerRunning || timerPaused)){
             timer.stop();
             hours = Integer.parseInt(hrsStr);
             minutes = Integer.parseInt(minStr);
@@ -215,6 +221,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             oldseconds = oldtime.get(2);
             olditerations = oldtime.get(3);
             calculateAverage();
+            //RESET THE TIMER TO 0
             timer.setBase(SystemClock.elapsedRealtime());
             pauseOffset = 0;
             timerPaused = false;
@@ -227,7 +234,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         }
 
     }
-
+    //FUNCTION FOR CALCULATING AVERAGE AND UPDATING THE DATABASE
     private void calculateAverage(){
         newiterations = olditerations + 1;
         newhours = oldhours + ((hours - oldhours) / newiterations);
@@ -243,7 +250,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         }
         taskdb.updateTask(selection,newhours,newminutes,newseconds,newiterations);
     }
-
+    //LOAD ALL THE TASKS FROM THE DATABASE
     private void DisplayTasksInTable(DatabaseHelper taskdb) {
         tableAdapter = new ArrayAdapter<TaskInfo>(MainActivity.this, android.R.layout.simple_list_item_1, taskdb.displayAll());
         taskList.setAdapter(tableAdapter);
@@ -253,4 +260,48 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
+
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle savedInstanceState) {
+
+        savedInstanceState.putBoolean("READY",timerReady);
+        savedInstanceState.putBoolean("RUNNING",timerRunning);
+        savedInstanceState.putBoolean("PAUSED",timerPaused);
+
+        savedInstanceState.putLong("BASE",timer.getBase());
+
+        savedInstanceState.putString("TIMER",currentTimerVal);
+//        savedInstanceState.putInt("TASK",currentTaskPosition);
+        savedInstanceState.putLong("OFFSET",pauseOffset);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    @Override
+    public void onRestoreInstanceState(@Nullable Bundle savedInstanceState) {
+
+        timerReady = savedInstanceState.getBoolean("READY");
+        timerRunning = savedInstanceState.getBoolean("RUNNING");
+        timerPaused = savedInstanceState.getBoolean("PAUSED");
+        currentTimerVal = savedInstanceState.getString("TIMER");
+        pauseOffset = savedInstanceState.getLong("OFFSET");
+        if(timerRunning || timerPaused){
+            timer.setBase(savedInstanceState.getLong("BASE") - pauseOffset);
+            if(timerPaused){
+                timer.setText(currentTimerVal);
+            }
+        }
+        super.onRestoreInstanceState(savedInstanceState);
+        if(timerRunning){
+            timer.start();
+        }
+        changeButtons();
+
+    }
 }
+
+//TUTORIALS AND RESOURCES
+//Stopwatch: https://www.youtube.com/watch?v=RLnb4vVkftc, https://stackoverflow.com/questions/4152569/how-to-change-format-of-chronometer/25391944, https://stackoverflow.com/questions/32079320/android-chronometer-save-time
+//SQLite Database: https://www.youtube.com/watch?v=hDSVInZ2JCs, https://www.youtube.com/watch?v=PA4A9IesyCg
+//ListView: https://www.youtube.com/watch?v=Mja5YoL9Jak
+//Dialog: https://www.youtube.com/watch?v=xaxG54VulKk
