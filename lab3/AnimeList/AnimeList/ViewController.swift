c3d84dfe490bc66ca811e1534da2b4a46119b082
alex-charland <66377521+alex-charland@users.noreply.github.com>
2021-02-15 23:27:20 -0700

    Actual lab 3

---
+++ lab3/AnimeList/AnimeList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimeList
+//
+//  Created by Alexandra Charland on 2/14/21.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
